3808a9d6c1cbf6b260c89d7e1518f81d0ba408c4
David Carrera Casado <mail@davidcarreracasado.cat>
2021-11-09 17:16:42 +0100

    Initial commit

---
+++ Makefile
@@ -0,0 +1,170 @@
+MAKEFLAGS := --warn-undefined-variables
+
+SRC_DIR := src
+BIN_DIR := bin
+OBJ_DIR := obj
+INCLUDE_DIR := include
+ASSETS_DIR := assets
+
+SOURCES := $(wildcard $(SRC_DIR)/*.c)
+
+OBJECTS_DEBUG := $(patsubst $(SRC_DIR)/%.c,$(OBJ_DIR)/%_dbg.o,$(SOURCES))
+OBJECTS_RELEASE := $(patsubst $(SRC_DIR)/%.c,$(OBJ_DIR)/%_rel.o,$(SOURCES))
+OBJECTS_DEVELOP := $(patsubst $(SRC_DIR)/%.c,$(OBJ_DIR)/%_dev.o,$(SOURCES))
+
+DEPENDS_DEBUG := $(OBJECTS_DEBUG:.o=.d)
+DEPENDS_RELEASE := $(OBJECTS_RELEASE:.o=.d)
+DEPENDS_DEVELOP := $(OBJECTS_DEVELOP:.o=.d)
+
+TARGETS := $(BIN_DIR)/main_dbg $(BIN_DIR)/main_rel $(BIN_DIR)/main_dev
+
+FONTS_FTD := $(ASSETS_DIR)/fonts/Cabin-Regular_36_latin-1.ftd
+FONTS_FTD += $(ASSETS_DIR)/fonts/CutiveMono-Regular_24_latin-1.ftd
+FONTS_PNG := $(patsubst $(ASSETS_DIR)/fonts/%.ftd,$(ASSETS_DIR)/textures/%.png,$(FONTS_FTD))
+
+
+CC := gcc
+
+CFLAGS := -I$(realpath $(INCLUDE_DIR)) -Ilib/thirty/include `pkg-config --cflags glfw3` `pkg-config --cflags cglm` -Wall -Wextra -Wfloat-equal -Wundef -Wshadow -Wpointer-arith -Wcast-align -Wmissing-prototypes -Wwrite-strings -Wcast-qual -Wswitch-default -Wswitch-enum -Wconversion -Wunreachable-code -Wimplicit-fallthrough -Wstringop-overflow=4 -std=c11
+LDFLAGS := `pkg-config --libs glfw3` `pkg-config --libs cglm` -lm -ldl -std=c11
+
+CFLAGS_DEBUG := -MMD -Og -g
+LDFLAGS_DEBUG := $(CFLAGS_DEBUG)
+CFLAGS_RELEASE := -MMD -DNDEBUG -flto -O2 -g
+LDFLAGS_RELEASE := $(CFLAGS_RELEASE)
+LDFLAGS_DEVELOP := -fsanitize=address -fsanitize=undefined -Og
+CFLAGS_DEVELOP := -MMD $(LDFLAGS_DEVELOP) -Werror
+
+define FIND_HEADERS_CMD
+( \
+    find . '(' -type d -name .git ')' -prune -o \
+           '(' -type d -name venv ')' -prune -o \
+           -type f '(' -name '*.c' -o -name '*.h' ')' -print; \
+)
+endef
+
+define FIND_SYSHEADERS_CMD
+( \
+    find /usr/include -type f -name '*.h' -print; \
+)
+endef
+
+.PHONY: dbg dev rel clearfonts clean veryclean purify impolute etags glad_rel glad_dbg fonts valgrind static-analysis
+
+rel: stb_img glad_rel fonts $(BIN_DIR)/main
+dev: stb_img glad_dbg fonts etags $(BIN_DIR)/main_dev
+dbg: stb_img glad_dbg fonts $(BIN_DIR)/main_dbg
+
+glad_rel:
+	make glad_rel -C lib/thirty
+glad_dbg:
+	make glad_dbg -C lib/thirty
+stb_img:
+	make include/stb_image.h -C lib/thirty
+
+clearfonts:
+	-rm -f $(FONTS_PNG)
+	-rm -rf $(ASSETS_DIR)/fonts
+clean: clearfonts
+	-rm -f $(OBJ_DIR)/*.o
+	-rm -rf $(BIN_DIR)
+	make clean -C lib/thirty
+veryclean: clean
+	-rm -rf $(OBJ_DIR)
+	-rm -f TAGS
+	make veryclean -C lib/thirty
+purify: clearfonts veryclean
+	-rm -f sysh_TAGS
+	make purify -C lib/thirty
+impolute: purify
+	-rm -rf venv
+	-rm -rf lib/BitmapFontGenerator/venv
+	make impolute -C lib/thirty
+
+etags: sysh_TAGS
+	-rm -f TAGS
+	$(FIND_HEADERS_CMD) | etags --include=$< -
+
+sysh_TAGS:
+	$(FIND_SYSHEADERS_CMD) | etags -o $@ -
+
+fonts: $(FONTS_FTD) $(FONTS_PNG)
+
+valgrind: $(BIN_DIR)/main_dbg
+	valgrind --leak-check=full           			\
+                 --show-leak-kinds=definite,indirect,possible	\
+	         --track-origins=yes         			\
+	         --verbose                   			\
+	         --log-file=valgrind-out.txt 			\
+	         $(BIN_DIR)/main_dbg
+
+lib/thirty/bin/thirty_dbg.a:
+	make dbg -C lib/thirty
+lib/thirty/bin/thirty.a:
+	make rel -C lib/thirty
+lib/thirty/bin/thirty_dev.a:
+	make dev -C lib/thirty
+
+$(BIN_DIR)/main_dbg: LDFLAGS += $(LDFLAGS_DEBUG)
+$(BIN_DIR)/main_rel: LDFLAGS += $(LDFLAGS_RELEASE)
+$(BIN_DIR)/main_dev: LDFLAGS += $(LDFLAGS_DEVELOP)
+
+$(BIN_DIR)/main_dbg: $(OBJECTS_DEBUG) lib/thirty/bin/thirty_dbg.a
+$(BIN_DIR)/main_rel: $(OBJECTS_RELEASE) lib/thirty/bin/thirty.a
+$(BIN_DIR)/main_dev: $(OBJECTS_DEVELOP) lib/thirty/bin/thirty_dev.a
+
+$(OBJ_DIR)/%_dbg.o: CFLAGS += $(CFLAGS_DEBUG)
+$(OBJ_DIR)/%_rel.o: CFLAGS += $(CFLAGS_RELEASE)
+$(OBJ_DIR)/%_dev.o: CFLAGS += $(CFLAGS_DEVELOP)
+
+$(OBJ_DIR)/%_dbg.o: $(SRC_DIR)/%.c
+	@mkdir -p $(OBJ_DIR)
+	$(CC) -c $(CFLAGS) -o $@ $<
+$(OBJ_DIR)/%_rel.o: $(SRC_DIR)/%.c
+	@mkdir -p $(OBJ_DIR)
+	$(CC) -c $(CFLAGS) -o $@ $<
+$(OBJ_DIR)/%_dev.o: $(SRC_DIR)/%.c
+	@mkdir -p $(OBJ_DIR)
+	$(CC) -c $(CFLAGS) -o $@ $<
+
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.c
+	@mkdir -p $(OBJ_DIR)
+	$(CC) -c $(CFLAGS) -o $@ $<
+
+$(TARGETS):
+	mkdir -p $(BIN_DIR)
+	$(CC) $(LDFLAGS) $^ -o $@
+
+$(BIN_DIR)/main: $(BIN_DIR)/main_rel
+	cp $< $@
+
+
+$(SRC_DIR)/.clang_complete $(INCLUDE_DIR)/.clang_complete: Makefile
+	echo $(CFLAGS) | tr " " "\n" > $@
+
+
+$(ASSETS_DIR)/fonts/Cabin-Regular.ttf:
+	mkdir -p $(ASSETS_DIR)/fonts
+	wget -q -O $@ "https://raw.githubusercontent.com/impallari/Cabin/master/fonts/TTF/Cabin-Bold.ttf"
+$(ASSETS_DIR)/fonts/CutiveMono-Regular.ttf:
+	mkdir -p $(ASSETS_DIR)/fonts
+	wget -q -O $@ "https://raw.githubusercontent.com/vernnobile/CutiveFont/master/CutiveMono/GWF-1.001/CutiveMono-Regular.ttf"
+
+$(ASSETS_DIR)/textures/Cabin-Regular_36_latin-1.png $(ASSETS_DIR)/fonts/Cabin-Regular_36_latin-1.ftd &: $(ASSETS_DIR)/fonts/Cabin-Regular.ttf lib/BitmapFontGenerator/venv
+	set -e; \
+	source lib/BitmapFontGenerator/venv/bin/activate; \
+	python lib/BitmapFontGenerator/BitmapFontGenerator.py --basedir $(ASSETS_DIR) --height 36 --encoding latin-1 $<
+$(ASSETS_DIR)/textures/CutiveMono-Regular_24_latin-1.png $(ASSETS_DIR)/fonts/CutiveMono-Regular_24_latin-1.ftd &: $(ASSETS_DIR)/fonts/CutiveMono-Regular.ttf lib/BitmapFontGenerator/venv
+	set -e; \
+	source lib/BitmapFontGenerator/venv/bin/activate; \
+	python lib/BitmapFontGenerator/BitmapFontGenerator.py --basedir $(ASSETS_DIR) --height 24 --encoding latin-1 $<
+
+lib/BitmapFontGenerator/venv: lib/BitmapFontGenerator/requirements.txt
+	virtualenv lib/BitmapFontGenerator/venv
+	source lib/BitmapFontGenerator/venv/bin/activate; \
+	pip install -r lib/BitmapFontGenerator/requirements.txt
+
+
+-include $(DEPENDS_DEBUG)
+-include $(DEPENDS_RELEASE)
+-include $(DEPENDS_DEVELOP)
---
+++ src/main.c
@@ -0,0 +1,190 @@
+#include <thirty/inputHelpers.h>
+#include <thirty/game.h>
+#include <thirty/util.h>
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+#define TITLE_BUFFER_SIZE 256
+#define FRAME_PERIOD_FPS_REFRESH 10
+#define CAMERA_PITCH_LIMIT_OFFSET 0.1F
+#define CAMERA_NEAR 0.1F
+#define CAMERA_FAR 100.0F
+#define CAMERA_FOV 45.0F
+
+static const float movement_speed = 10.0F;
+static const float look_sensitivity = 0.1F;
+
+static size_t camera_idx = 0;
+static struct fpsCameraController cam_ctrl;
+
+static void processMouseInput(void *registerArgs, void *fireArgs) {
+        struct game *game = registerArgs;
+        struct eventBrokerMousePosition *args = fireArgs;
+        const double xpos = args->xpos;
+        const double ypos = args->ypos;
+
+        struct scene *scene = game_getCurrentScene(game);
+        struct object *camera = scene_getObjectFromIdx(scene, camera_idx);
+        
+        const vec2s curr = {
+                .x = (float)xpos,
+                .y = (float)ypos,
+        };
+
+        static bool first = true;
+        static vec2s prev;
+        if (first) {
+                prev = curr;
+                first = false;
+        }
+
+        const vec2s offset = glms_vec2_sub(prev, curr);
+        fpsCameraController_look(&cam_ctrl, offset, game_timeDelta(game), camera);
+
+        prev = curr;
+}
+
+static void processKeyboardInput(void *registerArgs, void *fireArgs) {
+        struct game *game = registerArgs;
+        (void)fireArgs;
+        
+        vec2s movement = {.x=0, .y=0};
+        
+        if (game_keyPressed(game, GLFW_KEY_LEFT) ||
+            game_keyPressed(game, GLFW_KEY_A)) {
+                movement.x -= 1;
+        }
+        if (game_keyPressed(game, GLFW_KEY_RIGHT) ||
+            game_keyPressed(game, GLFW_KEY_D)) {
+                movement.x += 1;
+        }
+        
+        if (game_keyPressed(game, GLFW_KEY_UP) ||
+            game_keyPressed(game, GLFW_KEY_W)) {
+                movement.y += 1;
+        }
+        if (game_keyPressed(game, GLFW_KEY_DOWN) ||
+            game_keyPressed(game, GLFW_KEY_S)) {
+                movement.y -= 1;
+        }
+
+        struct scene *scene = game_getCurrentScene(game);
+        struct object *camera = scene_getObjectFromIdx(scene, camera_idx);
+
+        movement = glms_vec2_normalize(movement);
+        fpsCameraController_move(&cam_ctrl, movement, game_timeDelta(game), camera);
+}
+
+static void processKeyboardEvent(void *registerArgs, void *fireArgs) {
+        static bool playingQ = false;
+        static bool playingE = false;
+        
+        struct game *game = registerArgs;
+        struct eventBrokerKeyboardEvent *args = fireArgs;
+        
+        const int key = args->key;
+        const int action = args->action;
+        //const int modifiers = args->modifiers;
+                        
+        if (action == GLFW_PRESS) {
+                if (key == GLFW_KEY_ESCAPE) {
+                        game_shouldStop(game);
+                } else if (key == GLFW_KEY_F) {
+                        cam_ctrl.freefly = !cam_ctrl.freefly;
+                } else if (key == GLFW_KEY_E || key == GLFW_KEY_Q) {
+                        struct scene *scene = game_getCurrentScene(game);
+                        size_t idx = scene_idxByName(scene, "SnekSkin");
+                        struct object *object = scene_getObjectFromIdx(scene, idx);
+                        struct animationCollection *animationCollection = object_getComponent(object, COMPONENT_ANIMATIONCOLLECTION);
+                        if (key == GLFW_KEY_Q) {
+                                if (playingQ) {
+                                        animationCollection_poseAnimation(animationCollection, 0, 0);
+                                        playingQ = false;
+                                } else {
+                                        animationCollection_playAnimation(animationCollection, 0);
+                                        playingQ = true;
+                                }
+                        } else {
+                                if (playingE) {
+                                        animationCollection_poseAnimation(animationCollection, 1, 0);
+                                        playingE = false;
+                                } else {
+                                        animationCollection_playAnimation(animationCollection, 1);
+                                        playingE = true;
+                                }
+                        }
+                }
+        }
+}
+
+static void updateTitle(void *registerArgs, void *fireArgs) {
+        struct game *game = registerArgs;
+        (void)fireArgs;
+        
+        static unsigned count = 0;
+        if (count == FRAME_PERIOD_FPS_REFRESH) {
+                const int fps = (const int)(1.0F/game_timeDelta(game));
+                static char title[TITLE_BUFFER_SIZE];
+                snprintf(title, TITLE_BUFFER_SIZE, "[%d] Physics (%s)", fps,
+                         cam_ctrl.freefly ?
+                         "Freefly camera" :
+                         "FPS camera");
+                game_updateWindowTitle(game, title);
+                count = 0;
+        } else {
+                count++;
+        }
+}
+
+static void freeGame(void *registerArgs, void *fireArgs) {
+        struct game *game = registerArgs;
+        (void)fireArgs;
+
+        game_free(game);
+        free(game);
+}
+
+int main(void) {
+        static const vec4s black = GLMS_VEC4_BLACK_INIT;
+
+        // Initialize game
+        struct game *game = smalloc(sizeof(struct game));
+        game_init(game, SCREEN_WIDTH, SCREEN_HEIGHT, 1, 1);
+
+        // Read scene from file
+        struct scene *scene = game_createScene(game);
+        FILE *f = fopen("scenes/scene.bgl", "r");
+        scene_initFromFile(scene, game, f);
+        game_setCurrentScene(game, scene->idx);
+
+        // Setup camera
+        camera_idx = scene_idxByName(scene, "Camera");
+        fpsCameraController_init(&cam_ctrl, scene, movement_speed, look_sensitivity);
+
+        // Create UI
+        struct ui *ui = game_createUi(game);
+        ui_init(ui, SCREEN_WIDTH, SCREEN_HEIGHT);
+        ui_addQuad(ui, 10,10, 200,60, 0.0F, "ui_test_texture");
+        struct font *font = ui_getFont("CutiveMono-Regular", 24, "latin-1");
+        font_load(font);
+        ui_addText(ui, 35,45, 0.1F, (const unsigned char*)"Something.", font, black);
+        game_setCurrentUi(game, ui->idx);
+
+        scene_setSkybox(scene, "skybox");
+
+        // Register events
+        eventBroker_register(processKeyboardInput, EVENT_BROKER_PRIORITY_HIGH,
+                             EVENT_BROKER_KEYBOARD_INPUT, game);
+        eventBroker_register(processKeyboardEvent, EVENT_BROKER_PRIORITY_HIGH,
+                             EVENT_BROKER_KEYBOARD_EVENT, game);
+        eventBroker_register(processMouseInput, EVENT_BROKER_PRIORITY_HIGH,
+                             EVENT_BROKER_MOUSE_POSITION, game);
+        eventBroker_register(updateTitle, EVENT_BROKER_PRIORITY_HIGH,
+                             EVENT_BROKER_UPDATE, game);
+        eventBroker_register(freeGame, EVENT_BROKER_PRIORITY_HIGH,
+                             EVENT_BROKER_TEAR_DOWN, game);
+
+        // Main loop
+        game_run(game);
+        return 0;
+}
