08f7d492ab484237cab83c32fc168feb21d39926
David Carrera Casado <mail@davidcarreracasado.cat>
2022-03-02 21:54:03 +0100

    add FPS counter and ping to UI

--- src/main.c
+++ src/main.c
@@ -60,20 +60,73 @@ static void processKeyboardEvent(void *registerArgs, void *fireArgs) {
         }
 }
 
-static void updateTitle(void *registerArgs, void *fireArgs) {
+static void updateUI(void *registerArgs, void *fireArgs) {
         struct game *game = registerArgs;
-        (void)fireArgs;
-        
-        static unsigned count = 0;
-        if (count == FRAME_PERIOD_FPS_REFRESH) {
-                const int fps = (const int)(1.0F/game_timeDelta(game));
-                static char title[TITLE_BUFFER_SIZE];
-                snprintf(title, TITLE_BUFFER_SIZE, "[%d] Name goes here :)", fps);
-                game_updateWindowTitle(game, title);
-                count = 0;
-        } else {
+        struct eventBrokerUpdateUI *args = fireArgs;
+        struct nk_context *ctx = args->ctx;
+
+        static unsigned fps;
+        {
+                static float deltas = 0;
+                static unsigned count = 0;
+                deltas += ctx->delta_time_seconds;
+
                 count++;
+                if (count >= FRAME_PERIOD_FPS_REFRESH) {
+                        float avgDelta = deltas/FRAME_PERIOD_FPS_REFRESH;
+                        fps = (unsigned)(1/avgDelta);
+                        
+                        deltas = 0;
+                        count = 0;
+                }
         }
+        char fpsText[256];
+        snprintf(fpsText, 256, "fps: %u", fps);
+
+        char pingText[256];
+        if (game->server != NULL) {
+                snprintf(pingText, 256, "ping: %u", game->server->roundTripTime);
+        } else {
+                snprintf(pingText, 256, "ping: n/a");
+        }
+
+        if (nk_begin(ctx, "fps", nk_rect(0, 0, 100, 30), NK_WINDOW_NO_SCROLLBAR | NK_WINDOW_BACKGROUND | NK_WINDOW_NO_INPUT)) {
+                nk_layout_row_static(ctx, 10, 100, 1);
+                nk_label(ctx, fpsText, NK_TEXT_LEFT);
+                nk_label(ctx, pingText, NK_TEXT_LEFT);
+        }
+        nk_end(ctx);
+        
+        /* struct nk_colorf bg; */
+        /* bg.r = 0.10f, bg.g = 0.18f, bg.b = 0.24f, bg.a = 1.0f; */
+        /* if (nk_begin(ctx, "asdf title!", nk_rect(50,50,230,250), NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_SCALABLE|NK_WINDOW_MINIMIZABLE|NK_WINDOW_TITLE)) { */
+        /*         enum {EASY,HARD}; */
+        /*         static int op = EASY; */
+        /*         static int property = 20; */
+        /*         nk_layout_row_static(ctx, 30, 80, 1); */
+        /*         if (nk_button_label(ctx, "asdf button!")) { */
+        /*                 fprintf(stderr, "button pressed!!\n"); */
+        /*         } */
+        /*         nk_layout_row_dynamic(ctx, 30, 2); */
+        /*         if (nk_option_label(ctx, "easy", op == EASY)) op = EASY; */
+        /*         if (nk_option_label(ctx, "hard", op == HARD)) op = HARD; */
+        /*         nk_layout_row_dynamic(ctx, 25, 1); */
+        /*         nk_property_int(ctx, "Compression:", 0, &property, 100, 10, 1); */
+        /*         nk_layout_row_dynamic(ctx, 20, 1); */
+        /*         nk_label(ctx, "!b!a!ckground:", NK_TEXT_LEFT); */
+        /*         nk_layout_row_dynamic(ctx, 25, 1); */
+        /*         if (nk_combo_begin_color(ctx, nk_rgb_cf(bg), nk_vec2(nk_widget_width(ctx),400))) { */
+        /*                 nk_layout_row_dynamic(ctx, 120, 1); */
+        /*                 bg = nk_color_picker(ctx, bg, NK_RGBA); */
+        /*                 nk_layout_row_dynamic(ctx, 25, 1); */
+        /*                 bg.r = nk_propertyf(ctx, "#R:", 0, bg.r, 1.0f, 0.01f,0.005f); */
+        /*                 bg.g = nk_propertyf(ctx, "#G:", 0, bg.g, 1.0f, 0.01f,0.005f); */
+        /*                 bg.b = nk_propertyf(ctx, "#B:", 0, bg.b, 1.0f, 0.01f,0.005f); */
+        /*                 bg.a = nk_propertyf(ctx, "#A:", 0, bg.a, 1.0f, 0.01f,0.005f); */
+        /*                 nk_combo_end(ctx); */
+        /*         } */
+        /* } */
+        /* nk_end(ctx); */
 }
 
 int main(int argc, char *argv[]) {
@@ -88,6 +141,7 @@ int main(int argc, char *argv[]) {
         struct game *game = smalloc(sizeof(struct game));
         game_init(game, SCREEN_WIDTH, SCREEN_HEIGHT,
                   EVENT_TOTAL-EVENT_BROKER_EVENTS_TOTAL, 1);
+        game_updateWindowTitle(game, "Title goes here :)");
 
         // Read scene from file
         struct scene *scene = game_createScene(game);
@@ -118,8 +172,7 @@ int main(int argc, char *argv[]) {
         // Register events
         eventBroker_register(processKeyboardEvent, EVENT_BROKER_PRIORITY_HIGH,
                              EVENT_BROKER_KEYBOARD_EVENT, game);
-        eventBroker_register(updateTitle, EVENT_BROKER_PRIORITY_HIGH,
-                             EVENT_BROKER_UPDATE, game);
+        eventBroker_register(updateUI, EVENT_BROKER_PRIORITY_HIGH, EVENT_BROKER_UPDATE_UI, game);
 
         // Main loop
         game_run(game);
